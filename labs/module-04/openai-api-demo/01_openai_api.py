a65d1a9b15b8b2b5e6758cb385617db863deb230
asaravanakumar <saravana.anbarasu@gmail.com>
2026-06-15 11:31:24 +0530

    fix for OPENAI_API_KEY error

--- labs/module-04/openai-api-demo/01_openai_api.py
+++ labs/module-04/openai-api-demo/01_openai_api.py
@@ -9,7 +9,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # Ensure the API key is set in the
 MODEL_NAME = os.getenv("MODEL_NAME")  # Ensure the model name is set in the environment
 
 
-client = OpenAI(OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 response = client.responses.create(
     model=MODEL_NAME,
